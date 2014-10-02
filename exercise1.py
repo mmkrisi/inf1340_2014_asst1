b3689cec43d8b39546e27070038d0be31a49eba0
mmkrisi <kristina.mitova@gmail.com>
2014-10-02 17:02:09 -0400

    Testing commit

--- exercise1.py
+++ exercise1.py
@@ -45,7 +45,7 @@ def grade_to_gpa(grade):
     gpa = 0.0
     letter_grade_gpa_dictionary = {"A+": 4.0, "A": 4.0, "A-": 3.7, "B+": 3.3, "B": 3.0, "B-": 2.7, "FZ": 0.0}
     #list of top number in grade range
-    list_of_letter_grade = ["A+", "A", "A-", "B+", "B", "B-", "FZ"] #list of letter grades to use when input is an int
+    list_of_letter_grade = ["A+", "A", "A-", "B+", "B", "B-", "FZ"]#list of letter grades to use when input is an int
 
     if type(grade) is str:
         print ("letter") #remove this line once the code is implemented
@@ -54,7 +54,7 @@ def grade_to_gpa(grade):
         else:
             raise TypeError("Invalid letter grade passed as a parameter.")
     elif type(grade) is int:
-        print("mark") # remove this line once the code is implemented
+        print("mark")# remove this line once the code is implemented
         if 0 <= grade <= 100: # check that grade is in the accepted range
             if 90 <= grade <= 100: # convert the numeric grade to a letter grade
                 grade = list_of_letter_grade[0]
@@ -81,8 +81,8 @@ def grade_to_gpa(grade):
 
     # write a long if-statement to convert letter_grade
     # assign the value to gpa
-    if letter_grade == "A":
-        gpa = 4.0
+    if letter_grade in letter_grade_gpa_dictionary:
+        gpa = letter_grade_gpa_dictionary[grade]
 
     return gpa
 
