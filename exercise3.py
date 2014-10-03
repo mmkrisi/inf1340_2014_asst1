354536d68ca25101290400a98c6a51e9f097462c
mmkrisi <kristina.mitova@gmail.com>
2014-10-03 00:25:29 -0400

    Testing commit

--- exercise3.py
+++ exercise3.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
 def decide_rps(player1, player2):
+    """Returns the result of the rock, paper, scissors game
+
+    :param:
+        player1 (string): the choice of player1
+        player2 (string): the choice of player2
+        The strings accepted are only "Rock", "Paper" and "Scissor"
+
+    :return:
+        int: the result of the choices between player1 and player2's in one game:
+        If player 1 wins: 1
+        If player 2 wins: 2
+        If tie: 0
+
+    :raises:
+        TypeError if input is not one of the three strings given as choices for the game.
+    """
     return 1
 
 decide_rps = [("rock", "scissors"), ("scissors", "paper"), ("paper", "rock")]
@@ -12,4 +28,10 @@ def decide_rps(player1, player2):
         return "1"
     else:
         return player2 + "2"
+
+
+#Krisi
+result_game_dictionary = {"Rock": 0, "Paper": 1, "Scissors": 2} # I remember we need to use dictionaries for this exercise.
+
+
 
