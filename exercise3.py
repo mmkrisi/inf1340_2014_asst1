0ff1e6cedc2841d469ae4a2810e49495316bef0a
Halie Albertson <hlalbertson@gmail.com>
2014-10-02 22:21:27 -0400

    again, running the test fine but not sure

--- exercise3.py
+++ exercise3.py
@@ -4,15 +4,13 @@
 def decide_rps(player1, player2):
     return 1
 
-#1 is Player 1, 2 is Player 2
-
 decide_rps = [("rock", "scissors"), ("scissors", "paper"), ("paper", "rock")]
 
 def decide_rps(player1, player2):
     if player1 == player2:
-        return "Tie"
+        return "0"
     if ((player1, player2)) in decide_rps:
-        return player1 + "wins"
+        return "1"
     else:
-        return player2 + "wins"
+        return player2 + "2"
 
