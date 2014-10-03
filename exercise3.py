69f98360ca32af9ef5b6aa5d2cb5851ff9101c0a
Halie Albertson <hlalbertson@gmail.com>
2014-10-03 13:18:36 -0400

    I'm not really sure what else I need to do on this and how to do it, but I have the basis of the dictionary down. What we need to do now is return 0, 1, 2 depending on tie/win/lose.

--- exercise3.py
+++ exercise3.py
@@ -19,20 +19,36 @@ def decide_rps(player1, player2):
     """
     return 1
 
-decide_rps = [("rock", "scissors"), ("scissors", "paper"), ("paper", "rock")]
-
-def decide_rps(player1, player2):
-    if player1 == player2:
-        return "0"
-    if ((player1, player2)) in decide_rps:
-        return "1"
-    else:
-        return "2"
-
-
-#Krisi
-result_game_dictionary = {"Rock": 0, "Paper": 1, "Scissors": 2} # I remember we need to use dictionaries for this exercise.
+# decide_rps = [("rock", "scissors"), ("scissors", "paper"), ("paper", "rock")]
+#
+# def decide_rps(player1, player2):
+#     if player1 == player2:
+#         return "0"
+#     if ((player1, player2)) in decide_rps:
+#         return "1"
+#     else:
+#         return "2"
 
 
+# #Krisi
+# result_game_dictionary = {"Rock": 0, "Paper": 1, "Scissors": 2} # I remember we need to use dictionaries for this exercise.
 
+
+if type is (str):
+else
+
+
+rps_results = {}
+rps_results[("Stone", "Stone")] = 0
+rps_results[("Paper", "Paper")] = 0
+rps_results[("Scissors", "Scissors")] = 0
+rps_results[("Rock", "Scissors")] = 1
+rps_results[("Scissors", "Paper")] = 1
+rps_results[("Paper", "Rock")] = 1
+rps_results[("Rock", "Paper")] = 2
+rps_results[("Paper", "Scissors")] = 2
+rps_results[("Scissors", "Rock")] = 2
+
+
+print(rps_results[("Paper", "Stone")])
 
