81d01c7f3c98b4ac717f0b9fb7376e4ace677f47
Halie Albertson <hlalbertson@gmail.com>
2014-10-06 19:18:23 -0400

    Cleaned up the comment syntax

--- exercise3.py
+++ exercise3.py
@@ -22,24 +22,24 @@ def decide_rps(player1, player2):
 
     # make a dictionary, giving an int value to the type of choice
     result_game_dictionary = {"Rock": 1, "Paper": 2, "Scissors": 3}
-    #the logic is to apply mathematics when deciding who wins in each of the 9 possible outcomes
-    #check whether the input is a string
+    # the logic is to apply mathematics when deciding who wins in each of the 9 possible outcomes
+    # check whether the input is a string
     if type(player1) and type(player2) is not str:
-        #return a TypeError when the input is not a string
+    #return a TypeError when the input is not a string
         raise TypeError("Invalid type")
     else:
-        #check that the input is one of the three possible choices
+    # check that the input is one of the three possible choices
         if player1 in result_game_dictionary:
-            #asign the value corresponding to the choice as per the dictionary
+    # asign the value corresponding to the choice as per the dictionary
             player1_choice = result_game_dictionary[player1]
         else:
-            #return a ValueError when invalid string input for Player1
+    # return a ValueError when invalid string input for Player1
             raise ValueError("The value for Player1 is incorrect")
-        #check that the input is one of the three possible choices
+    # check that the input is one of the three possible choices
         if player2 in result_game_dictionary:
-            #asign the value corresponding to the choice as per the dictionary
+    # asign the value corresponding to the choice as per the dictionary
             player2_choice = result_game_dictionary[player2]
-            #check the combination through mathematical logic to find the winning Player
+    # check the combination through mathematical logic to find the winning Player
             game_result = player1_choice - player2_choice
             if game_result == 0:
                 return 0
@@ -48,7 +48,7 @@ def decide_rps(player1, player2):
             elif game_result == -1 or game_result == 2:
                 return 2
             else:
-                #return a ValueError when invalid string input for Player2
+    # return a ValueError when invalid string input for Player2
                 raise ValueError("The value for Player2 is incorrect")
 
 
